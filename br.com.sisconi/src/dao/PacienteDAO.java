96239d654ed5076da15adfca78e03e1151989272
ocfelipe <ocfelipe@gmail.com>
2013-04-06 16:00:17 -0300

    Mudança na inserção de paciente. Exclusão de co_endereço.

--- br.com.sisconi/src/dao/PacienteDAO.java
+++ br.com.sisconi/src/dao/PacienteDAO.java
@@ -42,8 +42,7 @@ public class PacienteDAO {
 					"'"+new Date(p.getDataNascimento().getTime())+"'" +
 					")";
 			
-			String sqlEndereco = "insert into tb_endereco (co_endereco, " +
-					"co_paciente, " +
+			String sqlEndereco = "insert into tb_endereco (co_paciente, " +
 					"ds_rua, " +
 					"ds_numero, " +
 					"ds_bairro, " +
@@ -51,8 +50,7 @@ public class PacienteDAO {
 					"ds_complemento, " +
 					"co_cidade) " +
 					"values " +
-					"((select count(temp.co_paciente)+1 from (select co_paciente from tb_endereco) as temp join tb_paciente on tb_paciente.co_paciente=temp.co_paciente where tb_paciente.nr_sus like '"+p.getNumeroSus()+"'), " +
-					"(select tb_paciente.co_paciente from tb_paciente where tb_paciente.nr_sus like '"+p.getNumeroSus()+"'), " +
+					"((select tb_paciente.co_paciente from tb_paciente where tb_paciente.nr_sus like '"+p.getNumeroSus()+"'), " +
 					"'"+p.getEnderecoRua()+"', " +
 					"'"+p.getEnderecoNumero()+"', " +
 					"'"+p.getEnderecoBairro()+"', " +
@@ -80,7 +78,7 @@ public class PacienteDAO {
 			Connection con = ConexaoBD.getInstancia().getConexao();
 			Statement stm = con.createStatement();
 			ResultSet res = stm.executeQuery("select tb_paciente.co_paciente, nr_sus, nr_cpf, nm_paciente, nm_mae, nr_telefone, dt_nascimento," +
-					" ds_rua, ds_numero, ds_bairro, ds_cep, ds_complemento, tb_cidade.nm_cidade, tb_estado.nm_estado" +
+					" ds_rua, ds_numero, ds_bairro, ds_cep, ds_complemento, tb_cidade.nm_cidade, tb_estado.nm_estado, tb_cidade.co_cidade, tb_estado.co_estado" +
 					" from tb_paciente join tb_endereco on tb_paciente.co_paciente = tb_endereco.co_paciente" +
 					" join tb_cidade on tb_endereco.co_cidade = tb_cidade.co_cidade" +
 					" join tb_estado on tb_cidade.co_estado = tb_estado.co_estado" +
@@ -89,19 +87,37 @@ public class PacienteDAO {
 			res.next();
 			p.setCodigoPaciente(res.getInt("tb_paciente.co_paciente"));
 			p.setNumeroSus(res.getString("nr_sus"));
-			p.setCpf(res.getString("nr_cpf"));
+			String cpf = res.getString("nr_cpf");
+			if (cpf.equals(null)) {
+				cpf="";
+			}
+			p.setCpf(cpf);
 			p.setNome(res.getString("nm_paciente"));
 			p.setNomeMae(res.getString("nm_mae"));
-			p.setNumeroTelefone(res.getString("nr_telefone"));
+			String telefone = res.getString("nr_telefone");
+			if (telefone.equals(null)) {
+				telefone="";
+			}
+			p.setNumeroTelefone(telefone);
 			String formatData = new SimpleDateFormat("dd/MM/yyyy").format(res.getDate("dt_nascimento"));
 			p.setDataNascimento(new SimpleDateFormat("dd/MM/yyyy").parse(formatData));
 			p.setEnderecoRua(res.getString("ds_rua"));
 			p.setEnderecoNumero(res.getInt("ds_numero"));
 			p.setEnderecoBairro(res.getString("ds_bairro"));
-			p.setEnderecoCEP(res.getString("ds_cep"));
-			p.setEnderecoComplemento(res.getString("ds_complemento"));
+			String cep = res.getString("ds_cep");
+			if (cep.equals(null)) {
+				cep="";
+			}
+			p.setEnderecoCEP(cep);
+			String complemento = res.getString("ds_complemento");
+			if (complemento.equals(null)) {
+				complemento="";
+			}
+			p.setEnderecoComplemento(complemento);
 			p.setEnderecoCidade(res.getString("tb_cidade.nm_cidade"));
 			p.setEnderecoEstado(res.getString("tb_estado.nm_estado"));
+			p.setEnderecoCodigoCidade(res.getInt("tb_cidade.co_cidade"));
+			p.setEnderecoCodigoEstado(res.getInt("tb_estado.co_estado"));
 			//ConexaoBD.getInstancia().fecharConexao();
 			return p;
 		} catch (SQLException e) {
@@ -111,6 +127,10 @@ public class PacienteDAO {
 			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	public boolean update(Paciente p) {
+		return false;
 	}
 	
 	public List<Estado> getEstados() {
